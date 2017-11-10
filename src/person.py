005dba73d1ff2821b844f2bdcc22bb44ea95b6f9
Eva <evajohnson714@gmail.com>
2017-11-10 17:55:30 +0300

    Addfiles

--- src/person.py
+++ src/person.py
@@ -5,7 +5,7 @@ class Person(object):
     as first_name, last_name and person type.
     '''
 
-    def __init__(self, person_name=None, person_type=None, wants_accomodation=False):
+    def __init__(self, person_name=None, person_type=None, wants_accomodation="N"):
         self.person_name = person_name
         self.person_type = person_type
         """The id() function returns unique integer(identity)
@@ -17,16 +17,16 @@ class Person(object):
 class Fellow(Person):
     """Creating class fellow which inherits its
     methods and properties from the superclass person"""
-    def __init__(self, person_name):
+    def __init__(self, person_name, wants_accomodation):
         super(Fellow, self).__init__(
             person_name, person_type='Fellow',
-            wants_accomodation=False)
+            wants_accomodation='Y')
 
 
 class Staff(Person):
     """Creating class staff which inherits its
     methods and properties from the superclass person"""
-    def __init__(self, person_name):
+    def __init__(self, person_name, wants_accomodation):
         super(Staff, self).__init__(
             person_name, person_type='Staff',
-            wants_accomodation=False)
+            wants_accomodation='N')
